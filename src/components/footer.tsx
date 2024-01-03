013f8ce8d7db114068397b0b8679abab9da3cc8c
David <davidrichardson1945@gmail.com>
2024-01-02 22:10:46 -0300

    refactor: remove unnecessary elements from sidebar

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,12 +6,12 @@ export default function Footer() {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a
-                        href="https://flowbite.com/"
+                        href="/"
                         className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
                     >
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                        <img src="/icon.png" className="h-8" alt="Flowbite Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                            Flowbite
+                            Linai
                         </span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -23,11 +23,6 @@ export default function Footer() {
                         <li>
                             <a href="#" className="hover:text-[#cc00ff] me-4 md:me-6">
                                 Privacy Policy
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:text-[#cc00ff] me-4 md:me-6">
-                                Licensing
                             </a>
                         </li>
                     </ul>
