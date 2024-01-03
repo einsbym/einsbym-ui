fc5ee8494c6157657e4403a2a27093f96512c9d7
David <davidrichardson1945@gmail.com>
2024-01-02 22:48:47 -0300

    refactor(navbar): set on hover colors for the menu button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
                         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                             <button
                                 type="button"
-                                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[#cc00ff1e] hover:text-[#cc00ff] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[#cc00ff]"
                                 aria-controls="mobile-menu"
                                 aria-expanded="false"
                                 onClick={() => setIsSidebarActive(true)}
