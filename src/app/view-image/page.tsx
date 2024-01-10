b28cb458af627f4d0825a5cd48f1ff1612234aee
David <davidrichardson1945@gmail.com>
2024-01-09 21:51:57 -0300

    feat(view-image): add image title and author information

--- src/app/view-image/page.tsx
+++ src/app/view-image/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import { useEffect, useState } from 'react';
 
@@ -17,9 +18,29 @@ export default function ViewImage() {
         <main className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
             <Navbar />
 
-            <div className="flex justify-center">
-                <img className="rounded-lg object-cover" src={selectedImage} />
+            <h2 className="mb-10 text-4xl font-extrabold dark:text-white underline underline-offset-3 decoration-8 decoration-[#cc00ff]">
+                Image title
+            </h2>
+            <img className="rounded-lg object-cover" src={selectedImage} />
+
+            <div className="flex items-center mt-5 p-5 border border-1 border-gray-500 rounded-lg">
+                <div className="flex-shrink-0">
+                    <img
+                        className="w-10 h-10 rounded-full"
+                        src="/profpic.jpeg"
+                        alt="Neil image"
+                    />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium truncate text-[#cc00ff]">Bianca Neri</p>
+                    <p className="text-sm text-gray-500 truncate">@biancaneri</p>
+                </div>
+                <div className="inline-flex text-xs items-center font-semibold text-gray-500">
+                    Há dois dias
+                </div>
             </div>
+
+            <Footer />
         </main>
     );
 }
