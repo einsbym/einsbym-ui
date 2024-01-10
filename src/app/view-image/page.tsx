063a53c17133db061660e55838f2b412dae3d203
David <davidrichardson1945@gmail.com>
2024-01-09 22:41:24 -0300

    feat(view-image): add tags and details section

--- src/app/view-image/page.tsx
+++ src/app/view-image/page.tsx
@@ -18,26 +18,42 @@ export default function ViewImage() {
         <main className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
             <Navbar />
 
-            <h2 className="mb-10 text-4xl font-extrabold dark:text-white underline underline-offset-3 decoration-8 decoration-[#cc00ff]">
+            <h2 className="mb-5 text-4xl font-extrabold dark:text-white underline underline-offset-3 decoration-8 decoration-[#cc00ff]">
                 Image title
             </h2>
+            <p className='mb-5'>
+                <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-1 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                    Default
+                </span>
+                <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-1 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                    Dark
+                </span>
+                <span className="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-1 rounded-full dark:bg-red-900 dark:text-red-300">
+                    Red
+                </span>
+            </p>
             <img className="rounded-lg object-cover" src={selectedImage} />
 
-            <div className="flex items-center mt-5 p-5 border border-1 border-gray-500 rounded-lg">
+            <div className="flex items-center mt-5 mb-5 p-5 border border-1 border-gray-500 rounded-lg">
                 <div className="flex-shrink-0">
-                    <img
-                        className="w-10 h-10 rounded-full"
-                        src="/profpic.jpeg"
-                        alt="Neil image"
-                    />
+                    <img className="w-10 h-10 rounded-full" src="/profpic.jpeg" alt="Bianca Neri" />
                 </div>
                 <div className="flex-1 min-w-0 ms-4">
                     <p className="text-sm font-medium truncate text-[#cc00ff]">Bianca Neri</p>
                     <p className="text-sm text-gray-500 truncate">@biancaneri</p>
                 </div>
-                <div className="inline-flex text-xs items-center font-semibold text-gray-500">
-                    Há dois dias
-                </div>
+                <div className="inline-flex text-xs items-center font-semibold text-gray-500">Há dois dias</div>
+            </div>
+
+            <h4 className="mb-5 text-2xl font-extrabold dark:text-white underline underline-offset-3 decoration-8 decoration-[#cc00ff]">
+                Details
+            </h4>
+            <div className="bg-gray-900 rounded-lg p-5">
+                <p>
+                    The paragraph element is one of the most commonly used HTML tags on a document page because it is
+                    used to write longer blocks of text separated by a blank line and can massively improve the on-page
+                    SEO of the page when used correctly.
+                </p>
             </div>
 
             <Footer />
