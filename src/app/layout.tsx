32b93c8219f00aca799cb3063caab00adf250da6
David <davidrichardson1945@gmail.com>
2024-01-10 20:16:12 -0300

    chore: change project name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Linai',
+    title: 'Einsbym',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,13 +5,10 @@ export default function Footer() {
         <footer className="shadow bg-gray-900 mt-10">
             <div className="w-full mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a
-                        href="/"
-                        className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-                    >
+                    <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <img src="/icon.png" className="h-8" alt="Flowbite Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                            Linai
+                            Einsbym
                         </span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -31,7 +28,7 @@ export default function Footer() {
                 <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
                     © {date.getFullYear()}{' '}
                     <a href="/" className="hover:text-[#cc00ff]">
-                        Linai
+                        Einsbym
                     </a>
                     . All Rights Reserved.
                 </span>
