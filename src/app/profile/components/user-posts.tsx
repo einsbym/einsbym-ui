132c5b9cb53dc5cf92f84115dc548a08f37755d8
David <davidrichardson1945@gmail.com>
2024-02-13 16:25:59 -0300

    feat(profile): refresh posts after publishing

--- src/app/profile/components/user-posts.tsx
+++ src/app/profile/components/user-posts.tsx
@@ -3,17 +3,24 @@ import { FIND_POSTS_BY_USER } from '@/graphql/queries/post';
 import { useQuery } from '@apollo/client';
 import { FaRegCommentAlt, FaRegHeart, FaRegShareSquare } from 'react-icons/fa';
 
-export default function UserPosts(props: { userId: string }) {
-    const { data } = useQuery(FIND_POSTS_BY_USER, {
-        variables: {
-            userId: props.userId
-        }
-    });
+export default function UserPosts(props: { userId: string, posts: any }) {
+    let posts = props.posts;
+
+    if (!posts) {
+        const { data } = useQuery(FIND_POSTS_BY_USER, {
+            variables: {
+                userId: props.userId
+            },
+            fetchPolicy: 'no-cache'
+        });
+    
+        posts = data?.findPostsByUser;
+    }
 
     return (
         <>
             {/* Text only posts */}
-            {data?.findPostsByUser.map((post: any) => (
+            {posts?.map((post: any) => (
                 <div key={post.id} className="mt-5 flex items-start gap-2.5">
                     <img
                         className="w-[60px] h-[60px] ring-2 p-1 ring-[#cc00ff] rounded-full object-cover"
@@ -22,12 +29,12 @@ export default function UserPosts(props: { userId: string }) {
                     />
                     <div className="flex flex-col w-full leading-1.5 p-4 border-gray-200 rounded-e-xl rounded-es-xl bg-gray-700">
                         <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                            <span className="text-sm font-semibold text-white">{post.user.firstName} {post.user.lastName}</span>
+                            <span className="text-sm font-semibold text-white">
+                                {post.user.firstName} {post.user.lastName}
+                            </span>
                             <span className="text-sm font-normal text-gray-400">11:46</span>
                         </div>
-                        <p className="text-sm font-normal py-2.5 text-white">
-                            {post.postText}
-                        </p>
+                        <p className="text-sm font-normal py-2.5 text-white">{post.postText}</p>
                         <div className="flex space-x-2 justify-end">
                             <button className="flex items-center gap-2 text-sm bg-gray-800 text-white rounded-full p-2 hover:bg-gray-200 hover:text-black transition duration-200">
                                 <FaRegHeart size={13} /> 232
