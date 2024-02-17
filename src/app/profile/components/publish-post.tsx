5d19ec0cfbd0a10d502fc79d211e78f87da3cf66
David <davidrichardson1945@gmail.com>
2024-02-17 16:01:41 -0300

    feat: post with image gallery

--- src/app/profile/components/publish-post.tsx
+++ src/app/profile/components/publish-post.tsx
@@ -1,16 +1,19 @@
 import { CREATE_POST } from '@/graphql/mutations/post';
 import { FIND_POSTS_BY_USER } from '@/graphql/queries/post';
 import { useLazyQuery, useMutation } from '@apollo/client';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { FiSend } from 'react-icons/fi';
 import { MdPostAdd } from 'react-icons/md';
 import UserPosts from './user-posts';
-import { Post } from '@/interfaces/interfaces';
+import { Image, Post } from '@/interfaces/interfaces';
+import { GrGallery } from 'react-icons/gr';
+import { storageServiceUrl } from '@/constants/constants';
 
 export default function PublishPost(props: { userId: string }) {
     // States
     const [postText, setPostText] = useState<string | null>();
     const [posts, setPosts] = useState<Post[]>([]);
+    const [files, setFiles] = useState<FileList | null>();
 
     // Mutations
     const [createPost] = useMutation(CREATE_POST);
@@ -18,27 +21,58 @@ export default function PublishPost(props: { userId: string }) {
     // Queries
     const [findPostsByUser] = useLazyQuery(FIND_POSTS_BY_USER);
 
+    const handleMultiFilesChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        setFiles(files);
+    };
+
     const handleSubmit = async (event: any) => {
         event.preventDefault();
 
         try {
             if (!postText) {
-                throw new Error("I mean... you need to write something, don't you agree?");
+                throw new Error("I mean... you need to write or upload something at least, don't you agree?");
             }
 
-            // Save image data
+            const images: any[] = [];
+
+            if (files) {
+                Array.from(files).forEach(async (file) => {
+                    // Save that shit in storage (or try to)
+                    const formData = new FormData();
+                    formData.append('file', file);
+        
+                    const response = await fetch(`${storageServiceUrl}/upload`, {
+                        method: 'POST',
+                        body: formData,
+                    });
+        
+                    if (response.status !== 200) {
+                        const { error } = await response.json();
+                        console.error(`The file ${file.name} could not be uploaded. Reason:`, error);
+                        return;
+                    }
+        
+                    // Get response from backend
+                    const responseInJson = await response.json();
+
+                    images.push({ filename: responseInJson.filename, name: "blah", description: "foobar", tags: [""] });
+                });
+            }
+
+            // Save post
             const { errors } = await createPost({
                 variables: {
                     createPostInput: {
                         userId: props.userId,
-                        images: [],
+                        images: images,
                         postText: postText,
                     },
                 },
             });
 
             if (errors) {
-                throw new Error('Error when attempting to publish the post.');
+                throw new Error('Error when attempting to publish your post.');
             }
 
             setPostText(null);
@@ -49,7 +83,7 @@ export default function PublishPost(props: { userId: string }) {
                 },
                 fetchPolicy: 'no-cache',
             });
-    
+
             setPosts(data?.findPostsByUser);
         } catch (error) {
             console.error('Something bad happened:', error);
@@ -72,6 +106,20 @@ export default function PublishPost(props: { userId: string }) {
                             value={postText || ''}
                             onChange={(event) => setPostText(event.target.value)}
                             required
+                        />
+                        <label
+                            className="text-white absolute end-12 bottom-2.5 hover:text-[#cc00ff] focus:outline-none px-4 py-2 cursor-pointer"
+                            htmlFor="fileInput"
+                            title='Select images'
+                        >
+                            <GrGallery size={20} />
+                        </label>
+                        <input
+                            multiple={true}
+                            className="hidden"
+                            id="fileInput"
+                            type="file"
+                            onChange={(event) => handleMultiFilesChange(event)}
                         />
                         <button
                             type="submit"
