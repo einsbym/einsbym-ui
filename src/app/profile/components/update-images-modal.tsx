1fd13b2c9c937caba51880589704f09f1843f26e
David <davidrichardson1945@gmail.com>
2024-03-03 23:40:25 -0300

    refactor: apply blur effect to modal component

--- src/app/profile/components/update-images-modal.tsx
+++ src/app/profile/components/update-images-modal.tsx
@@ -19,11 +19,11 @@ export default function UpdateImageModal(props: UpdateImageModalProps) {
             id="default-modal"
             className={`${
                 !props.isModalActive ? 'hidden' : null
-            } bg-[#000000be] overflow-y-auto overflow-x-hidden fixed top-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-screen max-h-full`}
+            } transition-all transform duration-200 overflow-y-auto overflow-x-hidden fixed top-0 left-0 z-10 flex justify-center items-center w-full md:inset-0 h-screen max-h-full`}
         >
             <div className="relative p-4 w-full max-w-2xl max-h-full">
                 {/* Modal content */}
-                <div className="relative rounded-lg shadow bg-gray-700">
+                <div className="relative rounded-lg shadow backdrop-filter backdrop-blur-lg bg-opacity-10 z-10 bg-black/30">
                     {/* Modal header */}
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-600">
                         <h3 className="text-xl font-semibold text-white">{props.modalName}</h3>
@@ -74,25 +74,30 @@ export default function UpdateImageModal(props: UpdateImageModalProps) {
                     </div>
 
                     {/* Modal footer */}
-                    <div className="flex items-center p-4 md:p-5 border-t rounded-b border-gray-600">
+                    <div className="flex gap-2 items-center p-4 md:p-5 border-t rounded-b border-gray-600">
                         <button
                             type="button"
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
                             onClick={props.handleSave}
+                            className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-purple-800"
                         >
-                            Save
+                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                save
+                            </span>
                         </button>
                         <button
                             type="button"
-                            className="ms-3 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600"
+                            className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-cyan-800"
                             onClick={() => {
                                 props.setIsModalActive(false);
                             }}
                         >
-                            Cancel
+                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                cancel
+                            </span>
                         </button>
+
                         {props.errorMessage && (
-                            <div className="ml-2 p-2 text-sm font-medium rounded-lg border border-red-400 text-red-400">
+                            <div className="p-2 text-sm font-medium rounded-lg border border-red-400 text-red-400">
                                 {props.errorMessage}
                             </div>
                         )}
--- src/app/profile/components/user-general-statistics.tsx
+++ src/app/profile/components/user-general-statistics.tsx
@@ -1,23 +1,22 @@
 export default function UserGeneralStatistics() {
     return (
-        <div className="w-11/12 lg:w-4/5 mx-auto mt-5 border rounded-lg shadow bg-gray-800 border-gray-700">
+        <div className="w-11/12 lg:w-4/5 mx-auto mt-5 rounded-lg shadow backdrop-filter backdrop-blur-lg bg-opacity-10 z-10 bg-black/30">
             <ul
-                className="text-sm font-medium text-center divide-x divide-gray-200 sm:flex divide-gray-600 text-gray-400 rtl:divide-x-reverse"
+                className="text-sm font-medium text-center sm:flex text-gray-400"
                 id="fullWidthTab"
             >
                 <li className="w-full">
                     <button
                         id="stats-tab"
                         type="button"
-                        role="tab"
-                        className="inline-block w-full p-4 rounded-ss-lg rounded-se-lg bg-gray-50 hover:bg-gray-100 focus:outline-none bg-gray-700 hover:bg-gray-600"
+                        className="inline-block w-full p-4 rounded-ss-lg rounded-se-lg focus:outline-none"
                     >
                         Statistics
                     </button>
                 </li>
             </ul>
-            <div id="fullWidthTabContent" className="border-t border-gray-200 border-gray-600">
-                <div className="p-4 rounded-lg md:p-8 bg-gray-800" id="stats">
+            <div id="fullWidthTabContent" className="border-t border-white">
+                <div className="p-4 rounded-lg md:p-8" id="stats">
                     <div className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto sm:grid-cols-3 xl:p-2 xl:grid-cols-6 text-white sm:p-8">
                         <div className="flex flex-col items-center justify-center">
                             <dt className="mb-2 text-3xl font-extrabold">2º</dt>
