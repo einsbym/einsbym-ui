c15652318b7fa855ec94e48baa0aef85e27ecf81
David <davidrichardson1945@gmail.com>
2024-03-08 00:47:22 -0300

    refactor: change style of login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,9 +2,8 @@
 
 import { SigninInput } from '@/interfaces/interfaces';
 import { AuthService } from '@/services/auth-config';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { ChangeEvent, MouseEvent, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { CiLogin } from 'react-icons/ci';
 import { FaRegHourglass } from 'react-icons/fa';
 import { MdError } from 'react-icons/md';
@@ -27,27 +26,21 @@ export default function Login() {
     };
 
     return (
-        <div className="flex flex-col items-center md:flex-row md:h-screen">
-            <div className="flex items-center justify-center w-full md:w-1/2">
-                <Image src="/bg.svg" alt="Login Image" width={800} height={600} style={{ width: 800, height: 600 }} />
-            </div>
-            <div
-                className={`${
-                    errorMessage ? 'lg:border lg:border-[#ff0000]' : null
-                } lg:bg-gray-900 p-10 rounded-lg flex flex-col items-center justify-center w-full md:w-1/4`}
-            >
-                <div className="w-full max-w-md space-y-8">
+        <div className="flex items-center justify-center md:h-screen bg-contain bg-cover bg-no-repeat bg-center bg-[url()]">
+            <div className="overflow-hidden rounded-lg flex items-center justify-center w-full md:w-1/2 md:h-4/6 backdrop-filter backdrop-blur-lg bg-opacity-10 z-10 bg-black/30 shadow-[0px_0px_25px_rgba(0,0,0,0.25)]">
+                <div className="w-full h-full bg-contain bg-cover bg-no-repeat bg-center bg-[url())]"></div>
+                <div className="w-full ml-10 mr-10 space-y-6">
                     <div>
                         <h1 className="text-2xl text-[#cc00ff] font-bold">Hi!</h1>
-                        <p className="mt-2 text-gray-600">Please sign in to your account.</p>
+                        <p className="mt-2 text-white">Please sign in to your account.</p>
                     </div>
-                    <form className="mt-5 space-y-6">
+                    <form className="">
                         <div className="relative z-0 w-full mb-5 group">
                             <input
                                 type="email"
                                 name="floating_email"
                                 id="email"
-                                className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:border-[#cc00ff] focus:outline-none focus:ring-0 peer"
+                                className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b appearance-none text-white border-white focus:border-[#cc00ff] focus:outline-none focus:ring-0 peer"
                                 placeholder=""
                                 autoComplete="on"
                                 required
@@ -55,7 +48,7 @@ export default function Login() {
                             />
                             <label
                                 htmlFor="email"
-                                className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#cc00ff] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#cc00ff] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                             >
                                 Email address
                             </label>
@@ -65,42 +58,40 @@ export default function Login() {
                                 type="password"
                                 name="floating_password"
                                 id="password"
-                                className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 text-white border-gray-600 appearance-none focus:outline-none focus:ring-0 focus:border-[#cc00ff] peer"
+                                className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b text-white border-white appearance-none focus:outline-none focus:ring-0 focus:border-[#cc00ff] peer"
                                 placeholder=""
                                 required
                                 onChange={(event) => handleChange(event)}
                             />
                             <label
                                 htmlFor="password"
-                                className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#cc00ff] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#cc00ff] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                             >
                                 Password
                             </label>
                         </div>
                         {errorMessage && (
-                            <div className="flex justify-center mt-5">
+                            <div className="flex justify-center mt-5 mb-5">
                                 <div className="flex gap-2 items-center w-fit p-2 text-[#ff0000] border border-[#ff0000] bg-[#ff00001a] rounded-lg">
                                     <MdError /> {errorMessage}
                                 </div>
                             </div>
                         )}
-                        <div>
-                            <button
-                                className="flex gap-2 items-center justify-center w-full text-white bg-gradient-to-r from-[#cc00ff] via-pink-500 to-[#cc00ff] hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 shadow-lg shadow-pink-500/50"
-                                onClick={(event) => signIn(event.preventDefault)}
-                                disabled={isLoading}
-                            >
-                                {isLoading ? (
-                                    <>
-                                        <FaRegHourglass size={20} /> Signing in...
-                                    </>
-                                ) : (
-                                    <>
-                                        <CiLogin size={30} /> Sign In
-                                    </>
-                                )}
-                            </button>
-                        </div>
+                        <button
+                            className="flex gap-2 items-center justify-center w-full text-white bg-gradient-to-r from-[#cc00ff] via-pink-500 to-[#cc00ff] hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 shadow-lg shadow-pink-500/50"
+                            onClick={(event) => signIn(event.preventDefault)}
+                            disabled={isLoading}
+                        >
+                            {isLoading ? (
+                                <>
+                                    <FaRegHourglass size={20} /> Signing in...
+                                </>
+                            ) : (
+                                <>
+                                    <CiLogin size={30} /> Sign In
+                                </>
+                            )}
+                        </button>
                     </form>
                 </div>
             </div>
