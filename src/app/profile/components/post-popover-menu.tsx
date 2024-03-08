e4e8a024fa3db77b820fd68d047fe8929849a886
David <davidrichardson1945@gmail.com>
2024-03-07 23:40:02 -0300

    refactor: change style of popover menu

--- src/app/profile/components/post-popover-menu.tsx
+++ src/app/profile/components/post-popover-menu.tsx
@@ -5,20 +5,20 @@ export default function PostPopoverMenu() {
     const [isEditBioActive, setIsEditBioActive] = useState<boolean>(false);
 
     return (
-        <div className="absolute right-0 bottom-[-130px] z-10 divide-y divide-gray-100 rounded-lg shadow w-44 bg-gray-700">
+        <div className="absolute right-0 bottom-[-130px] z-10 divide-y divide-gray-100 rounded-lg shadow w-44 backdrop-filter backdrop-blur-lg bg-opacity-10 z-10 bg-black/30">
             <ul className="py-2 text-sm text-gray-200">
                 <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-600 hover:text-white">
+                    <a href="#" className="block px-4 py-2 text-[#cc00ff] hover:bg-[#cc00ff1e]">
                         edit
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-600 hover:text-white">
+                    <a href="#" className="block px-4 py-2 text-[#cc00ff] hover:bg-[#cc00ff1e]">
                         change visibility
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="block px-4 py-2 hover:bg-gray-600 hover:text-white">
+                    <a href="#" className="block px-4 py-2 text-red-400">
                         delete
                     </a>
                 </li>
