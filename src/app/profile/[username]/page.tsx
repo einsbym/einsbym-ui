799b21738849f683e9927e44cba079856898b639
David <davidrichardson1945@gmail.com>
2024-03-13 22:46:30 -0300

    feat: check if usernames match

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -4,28 +4,42 @@ import Navbar from '@/components/navbar';
 import { FIND_USER_BY_USERNAME } from '@/graphql/queries/user';
 import { User } from '@/interfaces/interfaces';
 import { useQuery } from '@apollo/client';
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import { useCallback, useEffect, useState } from 'react';
 import UserBioAndPost from '../components/user-bio-and-post';
 import UserBlurredCover from '../components/user-blurred-cover';
 import UserCoverAndPhoto from '../components/user-cover-and-photo';
 import UserGallery from '../components/user-gallery';
 import UserGeneralStatistics from '../components/user-general-statistics';
+import { getCurrentUserFromCookie } from '@/actions/cookies';
 
 export default function UserProfile() {
+    const router = useRouter();
     const params = useParams<{ username: string }>();
     const [user, setUser] = useState<User | null>();
 
     const { data, loading } = useQuery(FIND_USER_BY_USERNAME, { variables: { username: params.username } });
 
+    // Check if usernames match
+    const checkUser = useCallback(async () => {
+        const user = await getCurrentUserFromCookie();
+
+        if (user?.username === params.username) {
+            console.log('Hey its you!');
+            router.push('/profile');
+        }
+
+        setUser(data.findUserByUsername);
+    }, [data, params, router]);
+
     useEffect(() => {
         if (data) {
-            setUser(data.findUserByUsername);
+            checkUser();
         }
-    }, [data]);
+    }, [data, checkUser]);
 
     if (loading) {
-        return loading;
+        return 'carregando...';
     }
 
     return (
