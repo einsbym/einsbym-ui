bb6a2bc772b98493632821f1801eb159761472b6
David <davidrichardson1945@gmail.com>
2024-04-16 20:20:41 -0300

    refactor: remove unused variable
    - Removed an unused variable from the storage service API.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,5 @@
 export const backend = {
     storageUrl: process.env.NEXT_PUBLIC_STORAGE_URL,
-    storageServiceUrl: process.env.NEXT_PUBLIC_STORAGE_SERVICE_URL,
     graphqlUrl: process.env.NEXT_PUBLIC_API_URL + '/graphql',
     restApiUrl: process.env.NEXT_PUBLIC_API_URL,
 };
