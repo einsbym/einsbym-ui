d8969cc83a56412940e3aeb3d582000bc5478fce
David Silva <davidrichardson1945@gmail.com>
2024-05-07 21:22:27 -0300

    feat: implement function to send posts to the server

--- src/app/blog/create/custom-editorjs.tsx
+++ src/app/blog/create/custom-editorjs.tsx
@@ -1,3 +1,5 @@
+import { getAccessTokenFromCookie } from '@/auth/cookies';
+import { backend } from '@/constants/constants';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 
@@ -12,22 +14,57 @@ const editor = new EditorJS({
 });
 
 export default function CustomEditorJs() {
-    const saveData = async () => {
-        editor
-            .save()
-            .then((outputData) => {
-                console.log('Article data: ', outputData);
-            })
-            .catch((error) => {
-                console.log('Saving failed: ', error);
+    const savePost = async () => {
+        try {
+            // Create an instance of `FormData`
+            const formData = new FormData();
+
+            // Append data
+            formData.append('title', 'yeeeeaaa');
+            formData.append('file', '');
+            formData.append('description', 'brooooo');
+            formData.append('tags', "['a', 'b', 'c']");
+
+            // Save data from Editor.JS
+            editor
+                .save()
+                .then((outputData) => {
+                    console.log('Article data: ', outputData);
+                    formData.append('body', JSON.stringify(outputData));
+                })
+                .catch((error) => {
+                    throw new Error(error);
+                });
+
+            // Get access token
+            const accessToken = await getAccessTokenFromCookie();
+
+            // Save post
+            const response = await fetch(`${backend.restApiUrl}/blog/create`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken?.value.replace(/^"(.*)"$/, '$1')}`,
+                },
+                body: formData,
             });
+
+            if (response.status !== 201) {
+                throw new Error(response.statusText);
+            }
+
+            const responseJson = await response.json();
+
+            console.log('All saved:', responseJson);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
         <button
             type="button"
             className="w-full bg-white text-black font-medium rounded-lg shadow-lg text-center p-2"
-            onClick={() => saveData()}
+            onClick={() => savePost()}
         >
             save text
         </button>
--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -8,7 +8,11 @@ export default function Create() {
     return (
         <div className="w-full flex items-center justify-center">
             <div className="w-1/2 mt-10">
-                <input className="p-5 bg-gray-900 rounded-lg shadow-lg focus:outline-none placeholder:text-gray-800" type="text" placeholder='Title' />
+                <input
+                    className="p-5 w-full bg-gray-900 rounded-lg shadow-lg focus:outline-none placeholder:text-gray-600"
+                    type="text"
+                    placeholder="Title"
+                />
                 <div id="editorjs" className="mt-5 p-5 bg-gray-900 rounded-lg shadow-lg">
                     <CustomEditorJs />
                 </div>
