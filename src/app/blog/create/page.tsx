78de673f2807f74cc1827c5411849d77fd2b8c44
nosdrahcirvsky <davidrichardson1945@gmail.com>
2024-05-13 22:15:47 -0300

    feat: add title to preview panel

--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 const CustomEditorJs: any = dynamic((): any => import('./custom-editorjs'), { ssr: false });
 
 export default function Create() {
+    const [title, setTitle] = useState<string>();
     const [data, setData] = useState<any[]>([]);
 
     return (
@@ -15,12 +16,14 @@ export default function Create() {
                     className="p-5 w-full bg-gray-900 rounded-lg shadow-lg focus:outline-none placeholder:text-gray-600"
                     type="text"
                     placeholder="Title"
+                    onChange={(event) => setTitle(event.target.value)}
                 />
                 <div id="editorjs" className="mt-5 p-5 bg-gray-900 rounded-lg shadow-lg">
                     <CustomEditorJs setData={setData} />
                 </div>
             </div>
             <div className="w-[700px] mt-10 ml-5 bg-gray-900 p-5">
+                {title && <h1 className='text-2xl text-md mb-5'>{title}</h1>}
                 {data.map((block) => (
                     <p key={block.id} className="mb-2" dangerouslySetInnerHTML={{ __html: `${block.data.text}` }}></p>
                 ))}
